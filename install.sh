18bbf2bfc8ddb96ae82df0e3b40310dbb9a1b019
Michael Büsch <m@bues.ch>
2025-10-31 21:31:16 +0100

    install: Error on root

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+
 basedir="$(realpath "$0" | xargs dirname)"
+
+die() {
+    echo "$*" >&2
+    exit 1
+}
+
+if [ "$(id -u)" = "0" ]; then
+    die "Must NOT be root to install."
+fi
+
 exec cargo install --path "$basedir"
+
 # vim: ts=4 sw=4 expandtab
